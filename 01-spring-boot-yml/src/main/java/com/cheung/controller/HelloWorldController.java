8b4fbb19cd79305cd69a66f5af35f5bc9fb4495d
Cheung <550690513@qq.com>
2018-07-13 11:53:15 +0800

    Code refactoring.

---
+++ 01-spring-boot-yml/src/main/java/com/cheung/controller/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.cheung.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Cheung
+ */
+@RestController
+public class HelloWorldController {
+
+	@Value(value = "${msg}")
+	private String msg;
+
+	@Value(value = "${author}")
+	private String author;
+
+	@Value("${randomInt}")
+	private Integer randomInt;
+
+
+	@RequestMapping("/hello")
+	public Map<String, Object> showHelloWorld() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("msg", this.msg);
+		map.put("author", this.author);
+		map.put("randomInt", this.randomInt);
+		return map;
+	}
+
+}
